75e3bf053de59a216b1ae3c5e7f77a06802484eb
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-12 15:20:02 -0300

    fix: problem with tests

--- src/data/write-fake-data-to-file.ts
+++ src/data/write-fake-data-to-file.ts
@@ -3,26 +3,19 @@ import * as config from "../config";
 import { generateFakeData } from "./generate-fake-data";
 import { progress } from "../libs/cli-progress";
 
-export async function writeFakeDataToFile(): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const stream = fs.createWriteStream(config.OUT_FILE_PATH);
-    progress.start(config.NUMBER_OF_DATA, 0);
+export async function writeFakeDataToFile() {
+  const stream = fs.createWriteStream(config.OUT_FILE_PATH);
+  progress.start(config.NUMBER_OF_DATA, 0);
 
-    Array.from({ length: config.NUMBER_OF_DATA }).forEach((_, index) => {
-      if (index % 100) {
-        progress.update(index + 1);
-      }
+  Array.from({ length: config.NUMBER_OF_DATA }).forEach((_, index) => {
+    if (index % 100) {
+      progress.update(index + 1);
+    }
 
-      const fakeData = generateFakeData();
-      stream.write(`${fakeData}\n`);
-    });
-
-    stream.on("error", (err) => {
-      reject(err);
-    });
-
-    stream.end();
-    progress.stop();
-    resolve();
+    const fakeData = generateFakeData();
+    stream.write(`${fakeData}\n`);
   });
+
+  stream.end();
+  progress.stop();
 }
